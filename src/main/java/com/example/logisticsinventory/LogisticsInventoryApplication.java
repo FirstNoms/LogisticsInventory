741da3f4f59c3b0e1e052f5dce1408882841f07a
Confidence Okere <okerenomso@gmail.com>
2022-01-11 16:13:01 +0100

    Logistics Inventory

---
+++ src/main/java/com/example/logisticsinventory/LogisticsInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.logisticsinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/logisticsinventory/LogisticsInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logisticsinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
